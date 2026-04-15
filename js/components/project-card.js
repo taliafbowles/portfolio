7d202b732fa27d76eca776c87ed3f369e89fc816
taliafbowles <93346631+taliafbowles@users.noreply.github.com>
2026-04-15 16:23:53 -0700

    Create project-card.js

---
+++ js/components/project-card.js
@@ -0,0 +1,20 @@
+function createProjectCard({ href, image, alt, category, year, title, description, tags }) {
+  return `
+    <a class="card" href="${href}">
+      <div class="card__image-wrap">
+        <img class="card__image" src="${image}" alt="${alt}" loading="lazy" />
+      </div>
+      <div class="card__body">
+        <div class="card__meta">
+          <span class="card__category">${category}</span>
+          <span class="card__year">${year}</span>
+        </div>
+        <h3 class="card__title">${title}</h3>
+        <p class="card__description">${description}</p>
+        <ul class="card__tags">
+          ${tags.map(tag => `<li class="card__tag">${tag}</li>`).join('')}
+        </ul>
+      </div>
+    </a>
+  `;
+}
